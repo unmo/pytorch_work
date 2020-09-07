11a0bcfe0de902fba86d8808601c2262d0b8fcc3
Tomoki Umezawa <tomoki.umezawa@jp.ricoh.com>
2020-09-07 14:25:58 +0900

    #doc

--- source/data/input_data.py
+++ source/data/input_data.py
@@ -9,7 +9,12 @@ class InputDataFactory:
     """Dataクラス生成クラス
     """
 
-    def __init__(self, data_class: "Data"):
+    def __init__(self, data_class: "TemplateData"):
+        """コンストラクタ
+
+        Args:
+            data_class (TemplateData): 抽象化されたDataクラスを継承しているクラス
+        """
         self.data = data_class
 
     def get_data(self):
@@ -19,11 +24,15 @@ class InputDataFactory:
 class TmplateData(metaclass=ABCMeta):
     @abstractmethod
     def get_data(self):
+        """データ取得のテンプレートメソッド
+        """
         pass
 
 
 class DummyInputData1(TmplateData):
     """ダミーのアルゴリズムIFの入力データクラス
+    任意の2次元配列を生成、取得可能
+
     アルゴリズムの機能テストに用いる想定
 
     Args:
@@ -31,6 +40,11 @@ class DummyInputData1(TmplateData):
     """
 
     def __init__(self, data_size: list):
+        """コンストラクタ
+
+        Args:
+            data_size (list): 任意の二次元配列のサイズ
+        """
         self.input_data = np.random.random([data_size[0], data_size[1]])
 
     def get_data(self):
@@ -39,6 +53,8 @@ class DummyInputData1(TmplateData):
 
 class InputData(TmplateData):
     """アルゴリズムIFの入力データクラス
+    音声を対象にするなら{Wave, mp3, ...}InputDataクラスになる
+    画像を対象にするなら{Jpeg, Png, ...}InputDataクラスになる
 
     Args:
         Data (Data): Dataクラスオブジェクト
@@ -46,6 +62,7 @@ class InputData(TmplateData):
 
     def __init__(self):
         """入力データ生成
+        どうやってデータ生成をするか
         """
         self.input_data = 2
 
