15fa6db51a985d6c6f7a3033973fd66c05484d89
Tomoki Umezawa <tomoki.umezawa@jp.ricoh.com>
2020-09-07 17:52:04 +0900

    いろいろなネットワーク構造を評価するため、可変なネットワーク定義を行う実装

--- source/model/autoencoder_net.py
+++ source/model/autoencoder_net.py
@@ -1,42 +1,104 @@
 # -*-coding: utf-8 -*-
 
-import numpy as np
 import torch
-from sklearn.datasets import fetch_openml
-from torch import nn, optim
-from torch.utils.data import DataLoader, TensorDataset
+
+# ネットワーク定義を動的に行うのはネットワークモデルの評価時のみにしか使わない
+# ネットワーク構造が確定したら、内部構造をコーディングすればよい
 
 
 class Encoder(torch.nn.Module):
-    def __init__(self, data_size, layer_num):
+    def __init__(self, dim: int, layer_num: int):
         super().__init__()
-        pass
+        self.init_dim = dim
+        self.layer_num = layer_num
+        self.constructions = {}
+        self.conbinate_relations = self.__get_conbinate_relation()
+
+        for layer, dims in enumerate(self.conbinate_relations, 1):
+            self.constructions[f"{layer}"] = torch.nn.Linear(dims[0], dims[1])
+            # exec(f"self.fc{layer} = torch.nn.Linear({dims[0]}, {dims[1]})")
+
+    def __get_conbinate_relation(self):
+        prev_dim = self.init_dim
+        relations = []
+        for layer in range(1, self.layer_num + 1):
+            if prev_dim == 1:
+                prev_dim *= 2
+            relations.append((prev_dim, prev_dim // 2))
+            prev_dim //= 2
+
+        return relations
 
     def forward(self, x):
+        for layer in range(1, len(self.conbinate_relations)):
+            x = self.constructions[f"{layer}"](x)
+            # x = eval(f"torch.relu(self.fc{layer}(x))")
         return x
 
 
 class Decoder(torch.nn.Module):
-    def __init__(self, data_size, layer_num):
+    def __init__(self, conbinate_relations: list):
+        """恒等写像なので、エンコーダ側の全結合層の関係を引数に渡す
+
+        Args:
+            conbination_relations (list): エンコード側の全結合層の関係
+        """
         super().__init__()
-        pass
+        self.constructions = {}
+        self.conbinate_relations = conbinate_relations[::-1]
+
+        for layer, dims in enumerate(self.conbinate_relations):
+            # self.constructions[f"{layer}"] = torch.nn.Linear(dims[0], dims[1])
+            exec(f"self.fc{layer} = torch.nn.Linear({dims[1]}, {dims[0]})")
 
     def forward(self, x):
+        for layer in range(1, len(self.conbinate_relations)):
+            x = eval(f"torch.relu(self.fc{layer}(x))")
         return x
 
 
 class StackedAutoEncoder(torch.nn.Module):
-    def __init__(self, data_size, layer_num):
+    def __init__(self, dim, layer_num):
         super().__init__()
-        self.enc = Encoder(data_size, layer_num)
-        self.dec = Decoder(data_size, layer_num)
+        self.enc = Encoder(dim, layer_num)
+        self.dec = Decoder(self.enc.conbinate_relations)
 
     def forward(self, x):
-        middle = self.enc(x)
-        x = self.dec(middle)
-
+        self.middle = self.enc(x)
+        x = self.dec(self.middle)
         return x
 
 
+class MyAutoEncoder:
+    def __init__(self, dim, layer, lossfunc, optimaizer):
+        self.dim = dim
+        self.layer = layer
+        self.optimaizer = optimaizer
+        self.stacked_ae = StackedAutoEncoder(dim, layer)
+
+    def train(self, train_data, epochs):
+        losses = []
+        output_and_label = []
+
+        for epoch in range(1, epochs + 1):
+            pass
+
+
 if __name__ == "__main__":
-    pass
+    # e = Encoder(256, 5)
+    # print(e.conbinate_relations)
+    # out = e.forward(torch.rand(2, 256))
+    # d = Decoder(e.conbinate_relations)
+    # out = d.forward(out)
+    # print(out.shape)
+    dim = 256
+    layer = 5
+    autoencder = StackedAutoEncoder(dim, layer)
+    out = autoencder.forward(torch.rand(10, dim))
+    print(out.shape)
+    print(autoencder.state_dict())
+
+    lossfunc = torch.nn.MSELoss()
+    optimaizer = torch.optim.SGD(autoencder.parameters(), lr=0.1)
+
+    my_ae = MyAutoEncoder(dim, layer, lossfunc, optimaizer)
